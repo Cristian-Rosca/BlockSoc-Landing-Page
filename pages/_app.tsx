8acd2e18e011d7111d98a80a943940babf9b48c3
Cristian Rosca <cristianrrosca@gmail.com>
2022-10-24 13:01:50 +0100

    page, background and header setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Box, ChakraProvider, extendTheme } from '@chakra-ui/react'
+import background from "../public/background.png"
+
+const theme = extendTheme({
+  styles: {
+    global: () => ({
+      body: {
+        bg: "black",
+        color: "white"
+      }
+    })
+  }
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+        <Box bgImage="../background.png" bgPosition="center" bgRepeat="repeat">
+        <Component  {...pageProps} />
+        </Box>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
