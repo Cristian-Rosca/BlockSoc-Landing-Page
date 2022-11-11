b7234b5299b2c71b5e601f8aec9afbae0df128c1
Cristian Rosca <cristianrrosca@gmail.com>
2022-11-11 18:28:27 +0000

    add margin to bottom

--- pages/index.tsx
+++ pages/index.tsx
@@ -117,7 +117,7 @@ const Home: NextPage = () => {
               <Text width={"60%"} textAlign={"center"} mt={"1rem"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
               </Text>
             </Box>
-            <Box display={"flex"} flexDirection={"row"} justifyContent={"space-evenly"} gap={"4rem"}>
+            <Box mb={"5rem"} display={"flex"} flexDirection={"row"} justifyContent={"space-evenly"} gap={"4rem"}>
               <Box mt={"5rem"} display={"flex"} flexDirection={"column"} bg={"rgba(52, 52, 52, 0.2)"} p={"3rem"}>
                 <Image src={discord_logo} width={"150px"} height={"150px"} />
                 <Heading mt={"3rem"} as='h4' size='l' >Dynamic NFT for students</Heading>
