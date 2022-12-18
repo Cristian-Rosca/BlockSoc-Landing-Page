beb75bfa88bdf8294089c09f9b99da3f4120f588
Cristian Rosca <cristianrrosca@gmail.com>
2022-12-18 16:01:33 +0000

    add responsive styling for CallToAction component

--- components/CallToActionContainer.tsx
+++ components/CallToActionContainer.tsx
@@ -1,42 +1,50 @@
-import { Box, Button, Container, Heading, Text } from "@chakra-ui/react";
+import { Box, Button, Container, Heading, Text, useMediaQuery } from "@chakra-ui/react";
 import Image from "next/image";
 import Link from "next/link";
 import discord_logo from "../public/discord.svg"
 
 
 const CallToActionContainer = () => {
-    return ( 
-        <Container mt={"8rem"} maxW='container.xl' bg={"#3D00EE"} rounded={10}>
-        <Box display={"flex"} flexDirection={"column"} justifyContent={"flex-start"} >
-          <Heading pt={"5rem"} px={"3rem"} as='h2' size='3xl' textAlign={"start"} >Help us build the next frontier of Web 3.0 talent.</Heading>
-          <Box display={"flex"} flexDirection={"row"} justifyContent={"space-evenly"} gap={"4rem"} mt={"3rem"}>
-            <Box display={"flex"} flexDirection={"column"} px={"3rem"}>
-              <Heading mt={"1rem"} as='h4' fontSize={"26"}>Student?</Heading>
-              <Text textStyle='primary' fontSize={"19"}>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
-              <Button textStyle='primary' mt={"3rem"} width={"auto"} p={"1.5rem"} bgColor={"purple.600"} alignSelf={"start"} >Join BlockSocDAO</Button>
-            </Box>
-            <Box display={"flex"} flexDirection={"column"} px={"3rem"}>
-              <Heading mt={"1rem"} as='h4' fontSize={"26"} >Company?</Heading>
-              <Text textStyle='primary' fontSize={"19"} >Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
-              <Button textStyle='primary' mt={"3rem"} width={"auto"} p={"1.5rem"} bgColor={"purple.600"} alignSelf={"start"} >Join BlockSocDAO</Button>
-            </Box>
 
-          </Box>
-          <Box display={"flex"} flexDirection={"row"} justifyContent={"start"} px={"3rem"} pt={"3rem"} pb={"2rem"}>
-            <Box display={"flex"} flexDirection={"column"} >
-              <Heading mt={"1rem"} as='h4' fontSize={"24"} marginTop={"2rem"}>Just want to hang out? Join our Discord</Heading>
-              <Box display={"flex"} justifyContent={"start"} my={"1rem"}>
-                <Link href="https://discord.gg/YRKHzc7ymb" >
-                  <a target="_blank">
-                    <Image src={discord_logo} height={"50px"} width={"50px"} />
-                  </a>
-                </Link>
-              </Box>
-            </Box>
+  const [isGreaterThan650px] = useMediaQuery([
+    '(min-width: 650px)',
+  ]);
+
+  return (
+
+    <Box display={"flex"} flexDirection={"column"} mt={"8rem"} justifyContent={"flex-start"} rounded={10} bg={"#3D00EE"} mx={isGreaterThan650px ? "10%" : "7%"} textAlign={isGreaterThan650px ? "start" : "center"}>
+      <Heading pt={isGreaterThan650px ? "5rem" : "3rem"} px={"5%"} as='h2' size={isGreaterThan650px ? "3xl" : "2xl"}  >Help us build the next frontier of Web 3.0 talent.</Heading>
+      <Box display={"flex"} flexDirection={isGreaterThan650px ? "row" : "column"} mt={"3rem"} px={"5%"} justifyContent={"space-between"} alignItems={isGreaterThan650px ? "" : "center"} gap={isGreaterThan650px ? "" : "3rem"}>
+        <Box display={"flex"} flexDirection={"column"} width={isGreaterThan650px ? "40%" : "90%"} alignItems={isGreaterThan650px ? "" : "center"}>
+          <Heading mt={"1rem"} as='h4' fontSize={"26"}>Student?</Heading>
+          <Text textStyle='primary' fontSize={"19"}>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+          <Box>
+            <Button textStyle='primary' mt={isGreaterThan650px ? "3rem" : "2rem"} p={"1.5rem"} bgColor={"purple.600"} >Join Discord</Button>
           </Box>
         </Box>
-      </Container>
-     );
+        <Box display={"flex"} flexDirection={"column"} width={isGreaterThan650px ? "40%" : "90%"} alignItems={isGreaterThan650px ? "" : "center"} >
+          <Heading mt={"1rem"} as='h4' fontSize={"26"} >Company?</Heading>
+          <Text textStyle='primary' fontSize={"19"} >Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+          <Box>
+            <Button textStyle='primary' mt={isGreaterThan650px ? "3rem" : "2rem"} p={"1.5rem"} bgColor={"purple.600"} >Contact Us</Button>
+          </Box>
+        </Box>
+      </Box>
+      <Box display={"flex"} flexDirection={"row"} justifyContent={"start"} px={"5%"} pt={"3rem"} pb={"2rem"}>
+        <Box display={"flex"} flexDirection={"column"} >
+          <Heading mt={"1rem"} as='h4' fontSize={isGreaterThan650px ? "1.5rem" : "1.3rem"} marginTop={"2rem"}>Just want to hang out? Join our Discord</Heading>
+          <Box display={"flex"} my={"1rem"} justifyContent={isGreaterThan650px ? "" : "center"}>
+            <Link href="https://discord.gg/YRKHzc7ymb" >
+              <a target="_blank">
+                <Image src={discord_logo} height={"50px"} width={"50px"} />
+              </a>
+            </Link>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+
+  );
 }
- 
+
 export default CallToActionContainer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -46,7 +46,7 @@ const Home: NextPage = () => {
 
 
 
-        {/* <CallToActionContainer /> */}
+        <CallToActionContainer />
         {/* <NFTTypesContainer /> */}
 
       </Box>
