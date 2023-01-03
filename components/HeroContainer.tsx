ed768f26d7b425a683abc45f18c446ea6536ab29
Cristian Rosca <cristianrrosca@gmail.com>
2023-01-03 18:33:59 +0000

    fix apostrophe bug in HeroContainer

--- components/HeroContainer.tsx
+++ components/HeroContainer.tsx
@@ -4,7 +4,7 @@ const HeroContainer = () => {
   return (
 
     <Flex flexDirection={"column"} justifyContent={"flex-start"} alignItems={"center"} mt={{ base: "5rem", lg: "8rem" }} px={{ base: "5%", md: "10%", lg: "15%" }}>
-      <Heading as={"h2"} size={{ base: "2xl", sm: "2xl", md: "3xl", lg: "4xl" }} lineHeight={"1.3rem"} textAlign={"center"}> Web3's premier talent development hub</Heading>
+      <Heading as={"h2"} size={{ base: "2xl", sm: "2xl", md: "3xl", lg: "4xl" }} lineHeight={"1.3rem"} textAlign={"center"}> Web3&apos;s premier talent development hub</Heading>
       <Text textStyle='primary' fontSize={"1.2rem"} textAlign={"center"} mt={"2rem"} width={"80%"}>BlockSocDAO is the central Web3 education and upskilling hub 
 designed for students interested in learning about and building a career in Web3. BlockSocDAO offers a suite of tools and features that enable web3 native employers to discover, verify, and recruit the best emerging Web3 talent.
       </Text>
